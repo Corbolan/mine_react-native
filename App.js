d239d6d094ccd92b7b98e08475ea36dffa57ae9c
Germano Flores Corbolan <gecorbolan@gmail.com>
2021-06-14 21:03:09 -0300

    Componente Mina(Mine)

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native'
 import params from './src/params';
 import Field from './src/Components/Field'
+import Mine from './src/Components/Mine'
 
 export default class App extends Component {
   render() {
@@ -16,6 +17,9 @@ export default class App extends Component {
         <Field opened nearMines={2}/>
         <Field opened nearMines={3}/>
         <Field opened nearMines={6}/>
+        <Field mined/>
+        <Field mined opened/>
+        <Field mined opened exploded/>
       </View>
     )
   }
