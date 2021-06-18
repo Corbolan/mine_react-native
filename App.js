81666579880eaee7e98648b84307e3c415f6f92c
Germano Flores Corbolan <gecorbolan@gmail.com>
2021-06-17 22:46:57 -0300

    Componente Flag #02

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react';
 import {StyleSheet, Text, View} from 'react-native'
 import params from './src/params';
 import Field from './src/Components/Field'
-import Mine from './src/Components/Mine'
-import Flag from './src/Components/Flag'
+// import Mine from './src/Components/Mine'
+// import Flag from './src/Components/Flag'
 
 export default class App extends Component {
   render() {
@@ -23,6 +23,8 @@ export default class App extends Component {
         <Field mined opened exploded/>
         <Field flagged />
         <Field flagged opened/>
+        {/* <Flag/>
+        <Flag bigger/> */}
       </View>
     )
   }
@@ -31,7 +33,9 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-    
+    flex: 1,
+    justifyContent:'center',
+    justifyContent:'space-around',
     marginTop: '20%',
     paddingHorizontal: '20%',
   },
